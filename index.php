0e8c7b2aca0c5394f7e304de11ea65c898de8117
Vasily Alexandrovich Okunevs <bubbletron@ya.ru>
2018-03-19 17:12:44 +0000

    Small changes

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <link rel="stylesheet" href="common/stylesheet.css">
+    
     <title>Schedule Dash</title>
   </head>
   <body style="margin: 20px;">
@@ -35,9 +35,9 @@
       <h1 id="digital-time">0:00</h1>
 
       <form>
-        <input type="text" name="s1" placeholder="(empty)"> <input style="width: 7%;" type="text" name="s1t"><br>
-        <input type="text" name="s2" placeholder="(empty)"> <input style="width: 7%;" type="text" name="s2t"><br>
-        <input type="text" name="s3" placeholder="(empty)"> <input style="width: 7%;" type="text" name="s3t">
+        <input type="text" name="s1" placeholder="(empty)"> <input style="width: 4.8rem;" type="text" name="s1t" placeholder="00:00"><br>
+        <input type="text" name="s2" placeholder="(empty)"> <input style="width: 4.8rem;" type="text" name="s2t" placeholder="00:00"><br>
+        <input type="text" name="s3" placeholder="(empty)"> <input style="width: 4.8rem;" type="text" name="s3t" placeholder="00:00">
       <form>
     </center>
 
