863cfcb62c40e372ebd1f36f73802c424710a347
Andrew Zenin <kashaket@yandex.ua>
2018-03-27 17:59:32 +0300

    Update javascript.js

--- common/javascript.js
+++ common/javascript.js
@@ -28,24 +28,38 @@ function updSession(){
 
 function addTask(){
 	var vals = [], arr = document.getElementsByClassName("alert alert-primary");
-	var last = 0;
+	var ntm = 1;
 	if( arr != null && arr.length != null && arr.length > 0 )
 	{
 		arr.forEach(function(it, i, arr){
 			vals[] = parseint(it.id.slice(3));
 		});
-		last = Math.max.apply(null, [1,3,5,-1,8,0]);
+		ntm = Math.max.apply(null, [1,3,5,-1,8,0]) + 1;
 	}
-	
+	var tsk = document.createElement("div"), tsked = document.createElement("div");
+	tsk.setAttribute("class", "alert alert-primary");
+	tsked.setAttribute("class", "alert alert-editor");
+	tsk.id = "tsk" + ntm.toString();
+	tsked.id = "tsked" + ntm.toString();
+	tsked.n = "Unspecified";
+	tsked.t = "24:00";
+	document.getElementById("_body").appendChild(tsk);
+	document.getElementById("_body").appendChild(tsked);
+	document.getElementById("_body").insertBefore(tsked, this);
+	document.getElementById("_body").insertBefore(tsk, tsked);
+	tsk.innerHtml = "<p>" + tsked.n + " <b>in " + tsked.t + "</b></p>";
 }
 
 function editTask(){
-	
+	var th = document.getElementById("tsked" + this.id.slice(2)); //id = ed(i) [ed1, ed2, etc]
+	th.n = th.children[0].value; //Edit 1 value
+	th.t = th.children[1].value + ":" + th.children[2].value; //Combobox1 + combobox2 values
+	document.getElementById("tsk" + this.id.slice(2)).innerHtml = "<p>" + th.n + " <b>in " + th.t + "</b></p>";
 }
 
 function deleteTask(){
-	document.getElementById("tsk" + this.id).remove();
-	document.getElementById("tsked" + this.id).remove();
+	document.getElementById( "tsk" + this.id.slice(2) ).remove();   //id = dt(i) [dt1, dt2, etc].
+	document.getElementById( "tsked" + this.id.slice(2) ).remove();
 	this.remove();
 	updSession();
 }
