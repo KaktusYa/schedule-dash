e836d3a7876e5acdaf93c494f9691cde6e562b30
Andrew Zenin <kashaket@yandex.ua>
2018-03-27 21:38:09 +0300

    Update and rename save_shedules.php to save_schedules.php

--- common/save_schedules.php
+++ common/save_schedules.php
@@ -3,6 +3,6 @@ if( isset( $_POST['content'] ) ){
 start_session();
 $e = @json_decode( $_POST['content'] );
 if( (json_last_error()===JSON_ERROR_NONE) && is_array($e) ){
-  $_SESSION['shedules'] = $_POST['content'];
+  $_SESSION['schedules'] = $_POST['content'];
 }
 ?>
